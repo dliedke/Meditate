077ce32f07f75fcaa6b7858f184cc1e86d669541
Florian Geigl <florian.geigl@gmail.com>
2025-03-03 11:13:32 +0100

    bufix

--- Meditate/makeRelease.sh
+++ Meditate/makeRelease.sh
@@ -6,6 +6,6 @@ SEDSTRING='s/about_AppVersion">.*</about_AppVersion">v'${VERSION}'</'
 echo "\n$SEDSTRING"
 grep -r ./ -e 'about_AppVersion">' | grep -v "bin" | grep -v "makeRelease.sh" | cut -f 1 -d ":" | xargs -n1 -I@ sed -i -e $SEDSTRING @
 
-SEDSTRING='0,/products/{s/version=".*"/version="'${VERSION}'"/}'
+SEDSTRING='/application/,/products/{s/version=".*"/version="'${VERSION}'"/}'
 echo "\n$SEDSTRING"
 sed -i -e $SEDSTRING ./manifest.xml 
