1a1310d01bc1299cbf612768c56a4f928e504201
Florian Geigl <florian.geigl@gmail.com>
2025-03-05 09:17:13 +0100

    bump version to v9.1.10

--- makeRelease.sh
+++ makeRelease.sh
@@ -3,6 +3,7 @@
 set -e
 VERSION=$1
 cd $(dirname "$0")
+cd Meditate
 SEDSTRING='s/about_AppVersion">.*</about_AppVersion">v'${VERSION}'</'
 echo "\n$SEDSTRING"
 grep -r ./ -e 'about_AppVersion">' | grep -v "bin" | grep -v "makeRelease.sh" | cut -f 1 -d ":" | xargs -n1 -I@ sed -i -e $SEDSTRING @
@@ -10,6 +11,8 @@ grep -r ./ -e 'about_AppVersion">' | grep -v "bin" | grep -v "makeRelease.sh" | 
 SEDSTRING='/application/,/products/{s/version=".*"/version="'${VERSION}'"/}'
 echo "\n$SEDSTRING"
 sed -i -e $SEDSTRING ./manifest.xml
+
+cd ..
 git add .
 git commit -am"bump version to v$VERSION"
 git tag "v$VERSION"
